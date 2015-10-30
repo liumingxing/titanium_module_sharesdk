070389acc81581304e48f003c37cf7548c43a008
刘明星 <liumingxing@mamashai.com>
2015-10-30 22:55:47 +0800

    首次提交

---
+++ iphone/Classes/ComMamashaiSharesdkModule.h
@@ -0,0 +1,14 @@
+/**
+ * titanium_module_sharesdk
+ *
+ * Created by Your Name
+ * Copyright (c) 2015 Your Company. All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@interface ComMamashaiSharesdkModule : TiModule
+{
+}
+
+@end
---
+++ iphone/Classes/ComMamashaiSharesdkModuleAssets.m
@@ -0,0 +1,24 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComMamashaiSharesdkModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation ComMamashaiSharesdkModuleAssets
+
+- (NSData*) moduleAsset
+{
+	
+
+	return nil;
+}
+
+- (NSData*) resolveModuleAsset:(NSString*)path
+{
+	
+
+	return nil;
+}
+
+@end
